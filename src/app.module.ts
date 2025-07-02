d6f70feae93766686dbc2d784831e530767a0c37
UlisesHdzOne <tu-email@example.com>
2025-07-02 16:31:58 -0600

    feat(core): configurar MailerModule con SMTP en AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.tu-servidor.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: '191244@ids.upchiapas.edu.mx',
+          pass: 'ulises209',
+        },
+      },
+      defaults: {
+        from: '"No Reply" <no-reply@tu-dominio.com>',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
