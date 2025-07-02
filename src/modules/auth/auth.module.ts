3aec529119d014a75d37e3a5f6a9435159377ea6
UlisesHdzOne <tu-email@example.com>
2025-07-02 14:11:34 -0600

    feat(auth): configurar módulo Auth con JWT y Prisma

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+
+import { PrismaService } from '../prisma/prisma.service';
+
+@Module({
+  imports: [
+    PrismaModule,
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '1h',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [PrismaService],
+  controllers: [],
+  exports: [],
+})
+export class AuthModule {}
